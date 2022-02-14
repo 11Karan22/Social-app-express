72bb49de6d4779101fd8fed319ce8281852e99b5
karan <kstkaranthakur03@gmail.com>
2022-02-14 12:39:38 +0530

    setting up db

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express=require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app=express();
 const port=8000;
+const db=require('./config/mongoose');
+app.use(express.static('./assets'));
+app.use(expressLayouts);
 app.use('/',require('./routes'));
+
 app.set('view engine','ejs');
 app.set('views','./views');
+
 app.listen(port,function(err)
 {
     if(err)
