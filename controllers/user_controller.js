916e9f646a011d5859ad78a2f0e7494c827e3126
karan <kstkaranthakur03@gmail.com>
2022-03-29 15:42:31 +0530

    manual authentication is done!

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,10 +1,32 @@
 const User=require('../models/user');//we have defined the schema over there
+
+//profile page 
 module.exports.profile=function(req,res)
 {
-
-    return res.render('user_profile',{
-        title:"Profile"
-    });
+   if(req.cookies.user_id)
+   {
+    User.findById(req.cookies.user_id,function(err,user)
+    {
+        if(err)
+        {
+            console.log('cannot find the user in profile segment');
+            return ;
+        }
+        if(user)
+        {
+        return res.render('user_profile',{
+            title:"hey i found the cookeyy!",
+            user:user
+        }) 
+       }
+       else{
+    
+       }
+    })
+   }
+   else{
+    return res.redirect('/user/sign-in');   
+   }
 }
 
 //getting sign-up data
@@ -29,11 +51,7 @@ module.exports.create=function(req,res)
     }
      User.findOne({email:req.body.email},function(err,user)//inbuilt function
      {
-         if(err)
-         {
-             console.log('err in finding user in signing up');
-             return;
-         }
+         
          if(!user)
          {
              User.create(req.body,function(err,user)
@@ -54,5 +72,31 @@ module.exports.create=function(req,res)
 //users toh ek step pehle hi 
 module.exports.createSession=function(req,res)
 {
-    //later
+    //find the user 
+   User.findOne({email:req.body.email},function(err,user)
+   {
+    if(err)
+    {
+        console.log('err in finding user in signing in');
+        return;
+    }
+   if(user)
+   {
+   //password check
+   if(user.password!=req.body.password)
+   {
+       return  res.redirect('back');
+   }
+   res.cookie('user_id',user.id);
+   return res.redirect('/user/profile');
+   }
+   else{
+   return res.redirect('back');
+   }
+
+   })
+
+
+
+
 }
--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,6 @@ router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.signIn);
 router.get('/profile',userController.profile);
 router.post('/create',userController.create);
+router.post('/create-session',userController.createSession);
+
 module.exports=router;
